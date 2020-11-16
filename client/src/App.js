de0d1b321fbe74e248c649e3aff9a8887c7da511
Aayush Joglekar <iit2017017@iiita.ac.in>
2020-11-16 19:49:02 +0530

    [Client] Set up Pixi and environment

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import { Stage, Sprite } from '@inlet/react-pixi';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 50,
+      y: 100
+    }
+    setInterval(() => {
+      this.movePlayer();
+    }, 500)
+  }
+
+  movePlayer() {
+
+    let newX = Math.floor(Math.random() * (window.innerWidth));
+    let newY = Math.floor(Math.random() * (window.innerHeight));
+
+    this.setState({ x: newX, y: newY });
+
+  }
+
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight + 4} options={{ backgroundColor: 0xEEEEEE, antialias: true }}>
+        <Sprite image="./bunny.png" x={this.state.x} y={this.state.y} />
+      </Stage>
+    );
+  }
 }
 
 export default App;
