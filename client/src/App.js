8116668e17fad78a3fa34afd3152ad84b90df0d7
Aayush Joglekar <iit2017017@iiita.ac.in>
2020-11-16 21:52:16 +0530

    [Client] Add keyboard to watch events

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Stage, Sprite } from '@inlet/react-pixi';
 import { Component } from 'react';
+import * as keyboardjs from 'keyboardjs';
 
 class App extends Component {
 
@@ -8,25 +9,40 @@ class App extends Component {
     super(props);
     this.state = {
       x: 50,
-      y: 100
+      y: 100,
+      height: window.innerHeight,
+      width: window.innerWidth
     }
-    setInterval(() => {
-      this.movePlayer();
-    }, 500)
   }
 
-  movePlayer() {
-
-    let newX = Math.floor(Math.random() * (window.innerWidth));
-    let newY = Math.floor(Math.random() * (window.innerHeight));
-
-    this.setState({ x: newX, y: newY });
-
+  componentDidMount() {
+    keyboardjs.watch();
+    keyboardjs.bind('', (e) => {
+      switch (e.code) {
+        case "ArrowLeft":
+          this.setState({ ...this.state, x: Math.max(0, this.state.x - (e.shiftKey ? 7 : 3)) });
+          break;
+        case "ArrowRight":
+          this.setState({ ...this.state, x: Math.min(this.state.width - 25, this.state.x + (e.shiftKey ? 7 : 3)) });
+          break;
+        case "ArrowUp":
+          this.setState({ ...this.state, y: Math.max(0, this.state.y - (e.shiftKey ? 7 : 3)) });
+          break;
+        case "ArrowDown":
+          this.setState({ ...this.state, y: Math.min(this.state.height - 25, this.state.y + (e.shiftKey ? 7 : 3)) });
+          break;
+        default:
+      }
+    });
+  }
+  
+  componentWillUnmount() {
+    keyboardjs.stop();
   }
 
   render() {
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight + 4} options={{ backgroundColor: 0xEEEEEE, antialias: true }}>
+      <Stage width={this.state.width} height={this.state.height} options={{ backgroundColor: 0x222222, antialias: true }}>
         <Sprite image="./bunny.png" x={this.state.x} y={this.state.y} />
       </Stage>
     );
