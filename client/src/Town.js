f59bd64f817948973b392d25b450438b328a0fba
Aayush Joglekar <iit2017017@iiita.ac.in>
2020-11-16 22:02:23 +0530

    [Client] Move Town to own component

---
+++ client/src/Town.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import * as keyboardjs from 'keyboardjs';
+import { Stage, Sprite } from '@inlet/react-pixi';
+
+class Town extends Component {
+
+    constructor() {
+        super();
+        // Initial state of the canvas
+        this.state = {
+            x: 50,
+            y: 100,
+            height: window.innerHeight,
+            width: window.innerWidth
+        }
+    }
+
+    // Is invoked when everything is set up and ready to launch
+    componentDidMount() {
+        // starts watching for keypresses
+        keyboardjs.watch();
+        keyboardjs.bind('', (e) => {
+            switch (e.code) {
+                case "ArrowLeft":
+                    this.setState({ ...this.state, x: Math.max(0, this.state.x - (e.shiftKey ? 7 : 3)) });
+                    break;
+                case "ArrowRight":
+                    this.setState({ ...this.state, x: Math.min(this.state.width - 25, this.state.x + (e.shiftKey ? 7 : 3)) });
+                    break;
+                case "ArrowUp":
+                    this.setState({ ...this.state, y: Math.max(0, this.state.y - (e.shiftKey ? 7 : 3)) });
+                    break;
+                case "ArrowDown":
+                    this.setState({ ...this.state, y: Math.min(this.state.height - 25, this.state.y + (e.shiftKey ? 7 : 3)) });
+                    break;
+                default:
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        keyboardjs.stop();
+    }
+
+    render() {
+        return (
+            <Stage width={this.state.width} height={this.state.height} options={{ backgroundColor: 0x222222, antialias: true }}>
+                <Sprite image="./bunny.png" x={this.state.x} y={this.state.y} />
+            </Stage>
+        );
+    }
+
+
+}
+
+export default Town;
