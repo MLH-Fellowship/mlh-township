3523d646eb0031676be2ebc465f91e4289a925c5
Aayush Joglekar <iit2017017@iiita.ac.in>
2020-11-17 03:39:21 +0530

    [client] Set up socketio

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Town from './Town';
 import { Component } from 'react';
+import io from 'socket.io-client';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      socketConn: null
+    }
+  }
+
+  componentDidMount() {
+      const socket = io("http://localhost:5000/");
+      this.setState({ socketConn: socket });
+  }
+
   render() {
     return (
-      <Town />
+      this.state.socketConn ? <Town conn={this.state.socketConn}/> : "Loading.."
     )
   }
 }
