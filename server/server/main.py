1fe82eb444634ba81f9e9f128d78fd67f805c6c7
awalvie <agrawal.vishesh.178@gmail.com>
2020-11-17 15:31:14 +0530

    [server] lots of changes
    - add routes for connect, disconnect and init
    - add classes for active_players and players

--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,12 @@
 from flask import current_app as app
+from flask import request
+from flask_socketio import emit
 
 from . import socketio
+from .acitve_players import ActivePlayers
+from .player import Player
 
-locations = {}
+ACTIVE_PLAYERS = ActivePlayers()
 
 
 @app.route('/')
@@ -10,6 +14,23 @@ def home():
     return {"hello": "connected"}
 
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ', message)
+@socketio.on('connect')
+def on_connect():
+    socket_id = str(request.sid)
+    player = Player(socket_id=socket_id)
+    ACTIVE_PLAYERS.add_player(player)
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+    socket_id = str(request.sid)
+    ACTIVE_PLAYERS.remove_player(socket_id)
+
+
+@socketio.on('init')
+def on_init(json):
+    socket_id = str(request.sid)
+    player = ACTIVE_PLAYERS.get_player(socket_id)
+    player.username = json['username']
+    player.update_axes(json['x'], json['y'])
+    emit('init', {'activePlayers': ACTIVE_PLAYERS.get_list()})
