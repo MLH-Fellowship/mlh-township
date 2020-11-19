f484b896c7294ef936ec5432f040731a942226eb
Gagan Deep <pandafy.dev@gmail.com>
2020-11-19 10:32:28 +0530

    [temp] Something

--- client/src/ChatBox.js
+++ client/src/ChatBox.js
@@ -7,17 +7,31 @@ class ChatBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            messages: [],
+            messages: [
+                {
+                    "from": {"username": "zerefwayne"},
+                    "message": "Hello how are you",
+                    "at": new Date().toDateString()
+                }
+            ],
             conn: props.conn,
-            inputText: "Hello"
+            inputText: "Hello",
+            global: true
         }
         this.renderMessages = this.renderMessages.bind(this);
         this.sendMessage = this.sendMessage.bind(this);
+        this.joinRoom = this.joinRoom.bind(this);
+        this.createRoom = this.createRoom.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
         this.state.conn.on('town/chat', (req) => {
+            this.setState({messages: [...this.state.messages, req]});
+        });
+
+        this.state.conn.on('room/chat', (req) => {
+            console.log('room/chat');
             this.setState({messages: [...this.state.messages, req]});
         });
     }
@@ -32,13 +46,30 @@ class ChatBox extends Component {
 
     sendMessage() {
         if(this.state.inputText) {
-            this.state.conn.emit('town/chat', this.state.inputText);
+            this.state.conn.emit(this.state.global ?'town/chat': 'room/chat', this.state.inputText);
             this.setState({inputText: ""});
         }
     }
 
-    handleChange(event) {
+    joinRoom() {
+        this.setState({global: false})
+        if(this.state.inputText) {
+            const roomName = this.state.inputText;
+            console.log('join', roomName);
+            this.state.conn.emit('room/join', {"name": roomName});
+        }
+    }
 
+    createRoom() {
+        this.setState({global: false})
+        if(this.state.inputText) {
+            const roomName = this.state.inputText;
+            console.log('create', roomName);
+            this.state.conn.emit('room/create', {"name": roomName});
+        }
+    }
+
+    handleChange(event) {
         this.setState({inputText: event.target.value});
     }
 
@@ -55,7 +86,9 @@ class ChatBox extends Component {
                 </div>
                 <div className="messageBox">
                     <textarea className="messageInput" value={this.state.inputText} onChange={this.handleChange}></textarea>
-                    <button onClick={() => {this.sendMessage()}} >Send</button>
+                    <button style={{marginRight: '.5rem'}} onClick={() => {this.sendMessage()}} >Send</button>
+                    <button style={{marginRight: '.5rem'}} onClick={() => {this.joinRoom()}} >Join</button>
+                    <button onClick={() => {this.createRoom()}} >Create</button>
                 </div>
             </div>
         )
